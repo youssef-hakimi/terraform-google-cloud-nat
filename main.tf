5c1aab57f7f3788376b832180918e4c8026ea677
hakimi11 <78730013+hakimi11@users.noreply.github.com>
2021-04-01 01:03:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,6 @@ resource "google_compute_router_nat" "main" {
   icmp_idle_timeout_sec               = var.icmp_idle_timeout_sec
   tcp_established_idle_timeout_sec    = var.tcp_established_idle_timeout_sec
   tcp_transitory_idle_timeout_sec     = var.tcp_transitory_idle_timeout_sec
-  enable_endpoint_independent_mapping = var.enable_endpoint_independent_mapping
 
   dynamic "subnetwork" {
     for_each = var.subnetworks
